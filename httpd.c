9d5f5313dfea4b67cfe22c364d6b0a0e4861b32f
78764279+Inifomeeo@users.noreply.github.com <78764279+Inifomeeo@users.noreply.github.com>
2024-12-30 09:11:10 +0000

    feat: serve static content

--- httpd.c
+++ httpd.c
@@ -1,10 +1,12 @@
 /* httpd.c*/
 
 #include <arpa/inet.h>
+#include <fcntl.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -14,12 +16,19 @@
 #define BACKLOG 5 // maximum number of pending connections
 
 // structure for HTTP request
-struct sHttpRequest
-{
+struct sHttpRequest {
     char method[8];
     char body[128];
 };
 typedef struct sHttpRequest httpreq;
+
+// structure for file
+struct sFile {
+    char name[64];
+    char *content;
+    int size;
+};
+typedef struct sFile File;
 
 // on success, returns a socket fd. on error, returns 0
 int srv_init(int portno)
@@ -108,8 +117,8 @@ httpreq *parse_http(char *str)
 char *cli_read(int s)
 {
     static char buf[512];
-    memset(buf, 0, sizeof(buf));
-    if (read(s, buf, sizeof(buf) - 1) < 0) {
+    memset(buf, 0, 512);
+    if (read(s, buf, 511) < 0) {
         perror("read");
         return 0;
     }
@@ -120,8 +129,8 @@ void http_response(int c, char *type, char *data)
     char buf[512];
     int n;
 
-    memset(buf, 0, sizeof(buf));
-    snprintf(buf, sizeof(buf) - 1, "Content-Type: %s\r\nContent-Length: %d\r\n\n%s\r\n", type, n, data);
+    memset(buf, 0, 512);
+    snprintf(buf, 511, "Content-Type: %s\r\nContent-Length: %d\r\n\n%s\r\n", type, n, data);
 
     n = strlen(buf);
     write(c, buf, n);
@@ -134,8 +143,8 @@ void http_header(int c, int code)
     char buf[512];
     int n;
 
-    memset(buf, 0, sizeof(buf));
-    snprintf(buf, sizeof(buf) - 1, "HTTP/1.1 %d OK\r\n", code);
+    memset(buf, 0, 512);
+    snprintf(buf, 511, "HTTP/1.1 %d OK\r\n", code);
 
     n = strlen(buf);
     write(c, buf, n);
@@ -143,11 +152,96 @@ void http_header(int c, int code)
     return;
 }
 
+// on success, returns a pointer to a file. on error, returns 0
+File *get_file(char *name)
+{
+    char buf[512];
+    char *p;
+    int n, x, fd;
+    File *f;
+
+    fd = open(name, O_RDONLY);
+    if (fd < 0) {
+        return 0;
+    }
+
+    f = malloc(sizeof(File));
+    if (!f) {
+        close(fd);
+        return 0;
+    }
+
+    strncpy(f->name, name, 63);
+    f->content = malloc(512);
+
+    x = 0;
+    while (1) {
+        memset(buf, 0, 512);
+        n = read(fd, buf, 512);
+        if (!n) {
+            break;
+        }
+        else if (x == -1) {
+            close(fd);
+            free(f->content);
+            free(f);
+            return 0;
+        }
+
+        //strncpy(f->content + x, buf, n);
+        memcpy((f->content) + x, buf, n);
+        x += n;
+        f->content = realloc(f->content, (x + 512));
+    }
+
+    f->size = x;
+    close(fd);
+
+    return f;
+}
+
+// on success, returns 1. on error, returns 0
+int send_file(int c, char *type, File *file)
+{
+    char buf[512];
+    char *p;
+    int n, x;
+
+    if (!file) {
+        return 0;
+    }
+
+    memset(buf, 0, 512);
+    snprintf(buf, 511, "Content-Type: %s\r\nContent-Length: %d\r\n\r\n", type, file->size);
+
+    n = file->size;
+    p = file->content;
+
+    while (1) {
+        x = write(c, p, (n < 512) ? n : 512);
+        if (x < 1) {
+            return 0;
+        }
+
+        n -= x;
+        if (n < 1) {
+            break;
+        }
+        else {
+            p += x;
+        }
+    }
+
+    return 1;
+}
+
 void cli_connect(int s, int c)
 {
     httpreq *req;
     char *p;
     char *res;
+    char str[182];
+    File *f;
 
     p = cli_read(c);
     if (!p) {
@@ -162,15 +256,38 @@ void cli_connect(int s, int c)
         close(c);
         return;
     }
+
+    if (!strcmp(req->method, "GET") && !strncmp(req->body, "/img/", 5)) {
+        if (strstr(req->body, "..")) {
+            res = "403 Forbidden";
+            http_header(c, 403);
+            http_response(c, "text/plain", res);
+        }
+        memset(str, 0, 182);
+        snprintf(str, 181, ".%s", req->body);
+        f = get_file(str);
+        if (!f) {
+            res = "404 Not Found";
+            http_header(c, 404);
+            http_response(c, "text/plain", res);
+        }
+        else {
+            http_header(c, 200);
+            if(!send_file(c, "image/png", f)) {
+                res = "500 Internal Server Error";
+                http_response(c, "text/plain", res);
+            }
+        }
+    }
     
     if (!strcmp(req->method, "GET") && !strcmp(req->body, "/app/webpage")) {
-        res = "<html><body><h1>Hello World</h1></body></html>";
+        res = "<html><body><img src=\"/img/1.png\" alt=\"image\"></body></html>";
         http_header(c, 200); // HTTP/1.1 200 OK
         http_response(c, "text/html", res);
     }
     else {
         res = "404 Not Found";
-        http_header(c, 404); // HTTP/1.1 404 Not Found
+        http_header(c, 404);
         http_response(c, "text/plain", res);
 
     }
